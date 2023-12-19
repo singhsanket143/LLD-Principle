524b8e2e237da608d75cdd7f41ae0c08f31dde0c
Sanket Singh <singhsanket143@gmail.com>
2023-12-19 20:40:43 +0530

    Added lesson 1 activity 4 solution

---
+++ src/Lesson1/Example4/BetterCode/ActivityLogger.java
@@ -0,0 +1,9 @@
+package Lesson1.Example4.BetterCode;
+
+public class ActivityLogger {
+
+    public void logUserActivity(String username) {
+
+    }
+
+}
---
+++ src/Lesson1/Example4/BetterCode/UserManager.java
@@ -0,0 +1,15 @@
+package Lesson1.Example4.BetterCode;
+
+public class UserManager {
+
+    // UserRepository userRepo;
+    public void createUser(String username) {
+        // Code to create a user
+        // this.userRepo.create(...);
+    }
+
+    public void deleteUser(String username) {
+        // Code to delete a user
+        // this.userRepo.destroy(...);
+    }
+}
