1b11ded89a719c4955a38ce35931d02a4953e454
Sanket Singh <singhsanket143@gmail.com>
2023-12-19 20:32:39 +0530

    Added lesson 1 activity 4

---
+++ src/Lesson1/Example4/ProblematicCode/UserManager.java
@@ -0,0 +1,15 @@
+package Lesson1.Example4.ProblematicCode;
+
+public class UserManager {
+    public void createUser(String username) {
+        // Code to create a user
+    }
+
+    public void deleteUser(String username) {
+        // Code to delete a user
+    }
+
+    public void logUserActivity(String username) {
+        // Code to log user's activity
+    }
+}
