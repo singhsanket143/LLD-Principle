87b5a4de5ec50cd1cc71aa02021ff3bb8d1715c7
Sanket Singh <singhsanket143@gmail.com>
2023-12-19 19:39:50 +0530

    Added lesson 1 activity 2

---
+++ src/Lesson1/Employee.java
@@ -0,0 +1,22 @@
+package Lesson1;
+
+public class Employee {
+
+    private int id;
+    public int getId() {
+        return this.id;
+    }
+
+    public String fetchBioData() {
+        return "Some bio data";
+    }
+
+    public double calculateSalary() {
+        return 0;
+    }
+
+    public void printPerformanceData() {
+        System.out.println("Some performance related data");
+    }
+
+}
