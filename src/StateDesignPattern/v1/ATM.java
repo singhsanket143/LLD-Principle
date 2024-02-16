c1fd13c4c83fae75ed1809a905445568eb567b9e
Sanket Singh <singhsanket143@gmail.com>
2024-02-16 22:12:31 +0530

    added codes for state v1.1

--- src/StateDesignPattern/v1/ATM.java
+++ src/StateDesignPattern/v1/ATM.java
@@ -12,10 +12,56 @@ public class ATM {
     }
 
     public String init() {
+        if(this.state.equals(ATMState.CARD_READING)) {
+            throw new IllegalArgumentException("old txn is in process, state: CARD_READING");
+        }
+        if(this.state.equals(ATMState.WITHDRAWL_DETAILS_READING)) {
+            throw new IllegalArgumentException("old txn is in process, state: WITHDRAWL_DETAILS_READING");
+        }
+        if(this.state.equals(ATMState.CARD_EJECTING)) {
+            throw new IllegalArgumentException("old txn is in process, state: CARD_EJECTING");
+        }
+        if(this.state.equals(ATMState.CASH_DISPENSING)) {
+            throw new IllegalArgumentException("old txn is in process, state: CASH_DISPENSING");
+        }
         // unique txn id creation
         String txnId = UUID.randomUUID().toString();
         this.state = ATMState.CARD_READING;
         return txnId;
+    }
+
+    public boolean cancelTxn(String txnId) {
+        if(this.state.equals(ATMState.CARD_READING)) {
+        }
+        if(this.state.equals(ATMState.WITHDRAWL_DETAILS_READING)) {
+        }
+        if(this.state.equals(ATMState.CARD_EJECTING)) {
+        }
+        if(this.state.equals(ATMState.CASH_DISPENSING)) {
+        }
+        return true;
+    }
+
+    public void readCard(String txnId, String cardType, String cardNum, String cvv) {
+        if(this.state.equals(ATMState.CARD_READING)) {
+        }
+        if(this.state.equals(ATMState.WITHDRAWL_DETAILS_READING)) {
+        }
+        if(this.state.equals(ATMState.CARD_EJECTING)) {
+        }
+        if(this.state.equals(ATMState.CASH_DISPENSING)) {
+        }
+    }
+
+    public void withdrawCash(String txnId, int amount) {
+        if(this.state.equals(ATMState.CARD_READING)) {
+        }
+        if(this.state.equals(ATMState.WITHDRAWL_DETAILS_READING)) {
+        }
+        if(this.state.equals(ATMState.CARD_EJECTING)) {
+        }
+        if(this.state.equals(ATMState.CASH_DISPENSING)) {
+        }
     }
 
 }
